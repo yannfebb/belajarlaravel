0d95ba9401b15ba9d0635dcd9e234330bd339d93
uryuuu <ikaru.h17@gmail.com>
2026-03-03 16:40:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});Route::get('/', function () {
+    return view('welcome');
+});Route::get('/', function () {
+    return view('welcome');
+});Route::get('/', function () {
+    return view('welcome');
+});Route::get('/', function () {
+    return view('welcome');
+});Route::get('/', function () {
+    return view('welcome');
+});
