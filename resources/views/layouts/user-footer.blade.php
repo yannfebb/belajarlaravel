e67c030787e887d556a2ba6220ba888f1b40d057
yannfebb <febilili5@gmail.com>
2026-06-06 20:40:48 +0700

    fix ui home
    miaw

--- resources/views/layouts/user-footer.blade.php
+++ resources/views/layouts/user-footer.blade.php
@@ -1,17 +1,20 @@
  <!-- Copyright Start -->
- <div class="container-fluid copyright bg-dark py-4">
+ <div class="container-fluid copyright bg-dark-custom py-4">
      <div class="container">
          <div class="row">
-             <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                 <span class="text-light"><a href="#"><i class="fas fa-copyright text-light me-2"></i>Your Site Name</a>, All right reserved.</span>
-             </div>
-             <div class="col-md-6 my-auto text-center text-md-end text-white">
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                <span class="text-light"><a href="https://www.instagram.com/yannfebb26?igsh=M3gyc2t6cWsyaXBk">
+                   <i class="fas fa-copyright text-light me-2"></i>YANNFEBB</a>, Follow Me.</span>
+                <span class="text-light"><a href="/about"><i class="text-light me-2"></i>Beliin kopi</a>, Help.</span>
+            </div>
+            <div class="col-md-6 my-auto text-center text-md-end text-custom">
                  <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
                  <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
                  <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                 Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a clas="border-bottom" href="https://themewagon.com">ThemeWagon</a>
-             </div>
-         </div>
-     </div>
+                Designed By <a class="border-bottom" >HTML Codex</a> Template
+                <a clas="border-bottom" href="https://themewagon.com">ThemeWagon</a>
+            </div>
+        </div>
+    </div>
  </div>
  <!-- Copyright End -->
