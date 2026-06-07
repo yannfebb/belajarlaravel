3ff89d0fb0e4d910d20bd08e5380d27125d85dfa
yannfebb <febilili5@gmail.com>
2026-06-07 13:44:16 +0700

    ngubah tulisan blog menjadi artikel dan membesarkan tulisan  about
    END?

--- resources/views/public/blog.blade.php
+++ resources/views/public/blog.blade.php
@@ -2,11 +2,11 @@
 @section('content')
   <div class="container-fluid page-header py-5 wow fadeIn" data-wow-delay="0.1s">
             <div class="container text-center py-5">
-                <h1 class="display-2 text-white mb-4">Our Blog</h1>
+                <h1 class="display-2 text-white mb-4">Article</h1>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb justify-content-center mb-0">
                         <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item text-white" aria-current="page">Our Blog</li>
+                        <li class="breadcrumb-item text-white" aria-current="page">Article</li>
                     </ol>
                 </nav>
             </div>
